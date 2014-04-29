78b0bbaed85f75b3a71c02e650e9a123d10a34ed
Miguel Lezama <miguel@libre.coop>
2014-04-29 16:58:09 -0300

    Use blog options to manage wizard and menu display.

--- jetpack-start.php
+++ jetpack-start.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Jetpack Start
 Plugin URI: https://github.com/automattic/jetpack-start
@@ -7,12 +6,31 @@ Description: Jetpack Start Wizard.
 Version: 0.1
 */
 
-if ( isset( $_GET['jetpackstart'] ) ) {
-	update_option( '_jetpackstart', (bool) $_GET['jetpackstart'] );
-}
-
-if ( get_option( '_jetpackstart' ) ) {
-	require_once( dirname( __FILE__ ) . '/class.jetpack-start.php' );
-	Jetpack_Start::init();
-}
-
+add_action( 'init', function() {
+	update_option( 'jpstart_menu', true );
+	if ( get_option( 'jpstart_wizard' ) ) {
+		if ( current_user_can( 'switch_themes' ) ) {
+			if ( is_blog_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+				require_once( __DIR__ . '/jetpack-start/class.jetpack-start.php' );
+				Jetpack_Start::init();
+			} else {
+				update_option( 'jpstart_menu', true );
+			}
+		}
+	}
+	if ( get_option( 'jpstart_menu' ) ) {
+		if ( current_user_can( 'switch_themes' ) ) {
+			require_once( __DIR__ . '/jetpack-start/class.jetpack-start.php' );
+			if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+				Jetpack_Start::init_menu_ajax();
+			} else // check for is_admin_bar_showing so it doesn't get displayed on the cutomizer.
+				if ( ( is_main_query()
+				       && ( is_admin_bar_showing() && ! is_blog_admin() && ! is_admin() ) )
+				     || isset( $_GET['jps_menu_action'] )
+				) {
+					delete_option( 'jpstart_wizard' );
+					Jetpack_Start::init_menu();
+				}
+		}
+	}
+});
