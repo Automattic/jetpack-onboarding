6fe59874ef743adeca7418238b61baa8adc7b5d4
Miguel Lezama <miguel@libre.coop>
2014-05-02 10:54:08 -0300

    Whitespaces

--- class.jetpack-start.php
+++ class.jetpack-start.php
@@ -141,7 +141,7 @@ class Jetpack_Start {
 		<div class="themes-box"></div>
 
 		<script id="themes_template" type="text/template">
-			<% _.each(themes,function(theme) { %>
+			<% _.each( themes, function( theme ) { %>
 				<div class="theme" data-theme="<%= theme.stylesheet %>" data-site_type="<%= site_type %>" style="background-image:url('<%= theme.img_preview %>');background-size: 100%;">
 					<div class="theme-buttons">
 						<a href="<%= theme.demo_url %>" class="button button-large theme-preview" target="_blank"><span class="small-icon fa fa-external-link"></span><?php _e( 'Preview', 'jetpack-start' ) ?></a>
