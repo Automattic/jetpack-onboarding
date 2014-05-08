3e35e0edd314342ff2c9c1c8eac3e183273a3333
Dave Martin <growthdesigner@gmail.com>
2014-05-07 22:38:17 -0400

    New 3 option modal front-end code
    3 options for the user once they land on their site.  This will pop up
    automatically.

---
+++ views/steps/options_modal.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Mockup of options modal</title>
+	<link rel="stylesheet" type="text/css" charset="utf-8"  media="screen, projection" href="../../css/jetpack-start-menu.css" />
+	<link rel="stylesheet" type="text/css" charset="utf-8"  media="screen, projection" href="../../css/font-awesome.css" />
+</head>
+<body>
+	<!-- START Mock toolbar - delete this -->
+	<div style="background: #222; height: 32px; left: 0; position: fixed; right: 0; top: 0; width: 100%;"></div>
+
+	<!-- START Modal code -->
+	<div class="nux-shade"></div>
+	<div class="nux-options">
+		<a href="#" class="close-nux-options"><span class="icon fa fa-times"></span></a>
+		<h1>Congrats on your new site!  What will you do next?</h1>
+		<a href="#" class="option next-step" data-link-type="new-post"><span class="big-icon fa fa-pencil-square-o"></span>Write my first post</a>
+		<a href="#" class="option next-step" data-link-type="new-post"><span class="big-icon fa fa-laptop"></span>Edit my site design</a>
+		<a href="#" class="option next-step" data-link-type="new-post"><span class="big-icon fa fa-refresh"></span>Choose a new design</a>
+	</div>
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script type="text/javascript" src="../../js/jetpack-start-menu.js"></script>
+</body>
+</html>
