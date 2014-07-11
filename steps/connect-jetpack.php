df9723fe9714c7f87e30f2b78d2bf8f1986ab180
Miguel Lezama <miguel@automattic.com>
2014-07-11 13:15:44 -0300

    Jetpack Step: Remove unnecessary singleton.
    The Jetpack class already does it.

--- steps/connect-jetpack.php
+++ steps/connect-jetpack.php
@@ -12,8 +12,6 @@ if (  isset( $_GET['page'] ) && $_GET['page'] == 'jetpack' && isset( $_GET['acti
 
 class Jetpack_Start_Step_connect_jetpack extends Jetpack_Start_Step {
 
-	var $jetpack;
-
 	function __construct() {
 		if ( class_exists( 'Jetpack' ) ) {
 			add_action( 'jetpack-start_step-connect-jetpack', array( $this, 'render' ) );
@@ -21,9 +19,6 @@ class Jetpack_Start_Step_connect_jetpack extends Jetpack_Start_Step {
 	}
 
 	function get_jetpack() {
-		if ( ! isset( $this->jetpack ) ) {
-			$this->jetpack = Jetpack::init();
-		}
-		return $this->jetpack;
+		return Jetpack::init();
 	}
 }
