4fba8dacbbbb673a9e7eededcc3884a471309720
Miguel Lezama <miguel@automattic.com>
2015-03-05 09:17:24 -0200

    Welcome Panel: Add Set title step

--- class.jetpack-start-welcome-panel.php
+++ class.jetpack-start-welcome-panel.php
@@ -2,13 +2,22 @@
 
 class Jetpack_Start_Welcome_Panel {
 
-	static function init() {
-		if ( isset( $_GET['jps_wp_action'] ) ) {
-			do_action( 'jetpack_start_welcome_panel_action', sanitize_text_field( $_GET['jps_wp_action'] ) );
-			wp_safe_redirect( remove_query_arg( 'jps_wp_action' ) );
-		}
+	const CHANGE_TITLE_KEY = 'change-title';
 
-		add_action('load-index.php', array( __CLASS__, 'init_welcome_panel' ) );
+	static function init() {
+		if ( current_user_can_for_blog( get_current_blog_id(), 'manage_options' ) ) {
+
+			if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+				self::init_ajax();
+			}
+
+			if ( isset( $_GET['jps_wp_action'] ) ) {
+				do_action( 'jetpack_start_welcome_panel_action', sanitize_text_field( $_GET['jps_wp_action'] ) );
+				wp_safe_redirect( remove_query_arg( 'jps_wp_action' ) );
+			}
+
+			add_action( 'load-index.php', array( __CLASS__, 'init_welcome_panel' ) );
+		}
 	}
 
 	static  function init_welcome_panel() {
@@ -16,7 +25,14 @@ class Jetpack_Start_Welcome_Panel {
 		if( $screen->base == 'dashboard' ) {
 			remove_action( 'welcome_panel', 'wp_welcome_panel' );
 			add_action( 'welcome_panel', array( __CLASS__, 'wp_welcome_panel' ) );
-			wp_enqueue_script( 'jetpack-start', plugins_url( 'js/welcome-panel.js', __FILE__ ), array( 'jquery' ) );
+			wp_register_script( 'jetpack-start', plugins_url( 'js/welcome-panel.js', __FILE__ ), array( 'jquery' ) );
+
+			$nonces = array(
+				'jps_change_title' => wp_create_nonce( self::CHANGE_TITLE_KEY )
+			);
+			wp_localize_script( 'jetpack-start', 'JPS', $nonces );
+
+			wp_enqueue_script( 'jetpack-start' );
 			wp_enqueue_style( 'jetpack-start', plugins_url( 'css/welcome-panel.css', __FILE__ ), array( 'wp-admin' ) );
 		}
 	}
@@ -28,6 +44,22 @@ class Jetpack_Start_Welcome_Panel {
 	static function wp_welcome_panel() {
 		require_once ( JETPACK_START_BASE_DIR . '/class.jetpack-start.php' );
 		Jetpack_Start::get_view( 'welcome-panel.php' );
+	}
+
+	static function init_ajax() {
+		if ( current_user_can_for_blog( get_current_blog_id(), 'switch_themes' ) ) {
+			add_action( 'wp_ajax_jps_change_title', array( __CLASS__, 'change_title' ) );
+		}
+	}
+
+	static function change_title() {
+		check_ajax_referer( self::CHANGE_TITLE_KEY, 'nonce' );
+		$title = esc_html( $_REQUEST['title'] );
+		if ( update_option( 'blogname', $title ) ) {
+			wp_send_json_success( $title );
+		} else {
+			wp_send_json_error();
+		}
 	}
 
 }
--- js/welcome-panel.js
+++ js/welcome-panel.js
@@ -1,9 +1,28 @@
-(function( $ ) {
-	$(document).ready(function() {
-		$('.welcome__section').click(function() {
-			$('.welcome__section.active').removeClass('active');
-			$(this).toggleClass('active');
-			return false;
-		});
-	});
-}) ( jQuery );
+(function ($) {
+    $(document).ready(function () {
+        var moveToNextSection = function (currentSection) {
+            currentSection.hide();
+            currentSection.next().show();
+            $( '.getting-started__steps ol li.current')
+                .removeClass('current')
+                .addClass('completed')
+                .next().addClass('current');
+        };
+        $('#welcome__site-title .submit input').click(function (event) {
+            var data, title = $('#site-title').val();
+            event.preventDefault();
+
+            if (!title || title.trim() === '') {
+                return;
+            }
+            data = {
+                action: 'jps_change_title',
+                nonce: JPS.jps_change_title,
+                title: title
+            };
+            $('#wp-admin-bar-site-name a').html(title);
+            $.post(ajaxurl, data);
+            moveToNextSection($('#welcome__site-title'));
+        });
+    });
+})(jQuery);
