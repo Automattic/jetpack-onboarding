0fa47fca37a8968f2d7f5870dfb8461836dcec24
Daniel Walmsley <goldsounds@gmail.com>
2015-05-12 08:11:48 -0700

    Add jps_reset=1 to reset wizard data

--- class.jetpack-start-welcome-panel.php
+++ class.jetpack-start-welcome-panel.php
@@ -11,6 +11,13 @@ class Jetpack_Start_WelcomePanel {
 	static function init_welcome_panel() {
 		$screen = get_current_screen();
 		if( $screen->base == 'dashboard' ) {
+
+			//reset data
+			if ( isset( $_GET['jps_reset'] ) ) {
+				delete_option( Jetpack_Start_EndPoints::STEP_STATUS_KEY );
+				delete_option( Jetpack_Start_EndPoints::STARTED_KEY );
+			}
+
 			//replace the usual welcome panel with our own
 			remove_action( 'welcome_panel', 'wp_welcome_panel' );
 			add_action( 'welcome_panel', array( __CLASS__, 'wp_welcome_panel' ) );
@@ -18,12 +25,12 @@ class Jetpack_Start_WelcomePanel {
 			// vars to inject into javascript
 			$jps_vars = Jetpack_Start_EndPoints::js_vars();
 
-			// JS
+			//IE-only shims
 			global $wp_scripts;
 			wp_register_script( 'ie-shims', plugins_url( 'js/ie-shims.js', __FILE__ ), array());
-			
 			$wp_scripts->add_data( 'ie-shims', 'conditional', 'lt IE 9' );
 			
+			//Core JS app
 			wp_register_script( 'jetpack-start', plugins_url( 'js/jetpack-start.js', __FILE__ ), array( 'jquery', 'underscore', 'wp-pointer', 'ie-shims' ) );
 			wp_localize_script( 'jetpack-start', 'JPS', $jps_vars );
 			wp_enqueue_script( 'jetpack-start' );
