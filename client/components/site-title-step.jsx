31ff16b57091bd257e17e70ca3e628080237b0b2
Jesse Friedman <jesserfriedman@gmail.com>
2015-11-25 18:51:43 -0500

    V1 changes to title screen
    removed the form field labels
    removed the form field descriptions
    removed underline below h4
    changed “description” to “tagline” based on core standards

--- client/components/site-title-step.jsx
+++ client/components/site-title-step.jsx
@@ -49,32 +49,10 @@ var SiteTitleStep = React.createClass({
 
 			<WelcomeSection id="welcome__site-title">
 				<h3>Let's launch your new website</h3>
-				<h4>Enter a Title and Description</h4>
-				<p style={styles.content}>
-					Make your site stand out with a catchy name.
-				</p>
+				<h4>Enter a Title and Tagline</h4>
 				<form onSubmit={this.handleSubmit}>
-					<table className="form-table">
-						<tbody>
-							<tr>
-								<th><label htmlFor="site_title">Site Title</label></th>
-								<td>
-									<input type="text" name="site_title" id="site-title" autoComplete="off" onChange={this.handleChangeTitle} value={this.state.title}
-					       placeholder="Site Title (this can be changed later)" required/>
-					       			<p className="description">The name of your site. This title appears at the top of pages and in search results.</p>
-					       		</td>
-					      	</tr>
-					      	<tr>
-								<th><label htmlFor="site_description">Site Description</label></th>
-								<td>
-									<input type="text" name="site_description" id="site-description" autoComplete="off" onChange={this.handleChangeDescription} value={this.state.description}
-					       placeholder="Site Description" required/>
-					       			<p className="description">A motto or tagline. This appears below the title on your site, and next to the title in search results. Try to be brief and descriptive, e.g. "Your online bait and tackle store"</p>
-					       		</td>
-							</tr>
-						</tbody>
-					</table>
-
+					<input type="text" name="site_title" id="site-title" autoComplete="off" onChange={this.handleChangeTitle} value={this.state.title} placeholder="Site Title (this can be changed later)" />
+					<input type="text" name="site_description" id="site-description" autoComplete="off" onChange={this.handleChangeDescription} value={this.state.description} placeholder="Tagline (this can be changed later)" />
 					<p className="submit">
 						<Button color="blue">Next Step &rarr;</Button>
 					</p>
