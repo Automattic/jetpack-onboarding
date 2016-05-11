24fbb01f700ba768778c8092020a65a3a5aac915
Jesse Friedman <jesserfriedman@gmail.com>
2016-05-11 09:41:20 -0400

    Improved layout and verbiage of getting started question

--- client/components/steps/get-started.jsx
+++ client/components/steps/get-started.jsx
@@ -39,15 +39,13 @@ var GetStarted = React.createClass({
 			<div className="welcome__get-started--intro">
 				<div className="welcome__get-started--wrapper">
 					<h1>Welcome to WordPress</h1>
-					<p className="welcome__callout welcome__get-started--callout">Would you like help launching your site?</p>
+					<p className="welcome__callout welcome__get-started--callout">What kind of site can we help you set up?</p>
 					<p>
-						<Button onClick={ this.handleGetStarted.bind(this, "business") } primary>Yes, I’m building a business site</Button>
+						<Button onClick={ this.handleGetStarted.bind(this, "business") } primary>Business</Button>
+						<Button onClick={ this.handleGetStarted.bind(this, "personal") } primary>Personal</Button>
 					</p>
 					<p>
-						<Button onClick={ this.handleGetStarted.bind(this, "personal") } primary>Yes, I’m building a personal site</Button>
-					</p>
-					<p>
-						<Button onClick={ this.handleNoThanks }>No thanks</Button>
+						<Button onClick={ this.handleNoThanks }>I don't need help</Button>
 					</p>
 				</div>
 				<img className='welcome__get-started-image' src={ `${ JPS.base_url }/img/jpo-welcome.png` } />
