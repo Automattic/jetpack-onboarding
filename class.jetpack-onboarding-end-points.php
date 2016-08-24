279d2731b8020d584d90814ed6bd1b19648ba4d7
Marin Atanasov <tyxla@abv.bg>
2016-08-24 16:07:35 +0300

    Business step: Introduce method for adding a widget to sidebar

--- class.jetpack-onboarding-end-points.php
+++ class.jetpack-onboarding-end-points.php
@@ -548,6 +548,29 @@ Warwick, RI 02889
     	return false;
 	}
 
+	static function insert_widget_in_sidebar( $widget_id, $widget_data, $sidebar ) {
+		// Retrieve sidebars, widgets and their instances
+		$sidebars_widgets = get_option( 'sidebars_widgets', array() );
+		$widget_instances = get_option( 'widget_' . $widget_id, array() );
+
+		// Retrieve the key of the next widget instance
+		$numeric_keys = array_filter( array_keys( $widget_instances ), 'is_int' );
+		$next_key = $numeric_keys ? max( $numeric_keys ) + 1 : 2;
+
+		// Add this widget to the sidebar
+		if ( ! isset( $sidebars_widgets[ $sidebar ] ) ) {
+			$sidebars_widgets[ $sidebar ] = array();
+		}
+		$sidebars_widgets[ $sidebar ][] = $widget_id . '-' . $next_key;
+
+		// Add the new widget instance
+		$widget_instances[ $next_key ] = $widget_data;
+
+		// Store updated sidebars, widgets and their instances
+		update_option( 'sidebars_widgets', $sidebars_widgets );
+		update_option( 'widget_' . $widget_id, $widget_instances );
+	}
+
 	static function add_business_address() {
 		check_ajax_referer( self::AJAX_NONCE, 'nonce' );
 		$active_sidebars = get_option( 'sidebars_widgets' );
