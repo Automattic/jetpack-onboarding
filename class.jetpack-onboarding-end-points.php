3909908f9b1538048747c2f00f02fd0bf35c87f5
roccotripaldi <tripaldir@gmail.com>
2016-10-27 13:32:50 -0400

    Version bump to 1.5

--- class.jetpack-onboarding-end-points.php
+++ class.jetpack-onboarding-end-points.php
@@ -9,7 +9,7 @@ class Jetpack_Onboarding_EndPoints {
 	const BUSINESS_ADDRESS_SAVED_KEY = 'jpo_business_address_saved';
 	const MAX_THEMES = 3;
 	const NUM_RAND_THEMES = 3;
-	const VERSION = "1.4";
+	const VERSION = "1.5";
 	const WOOCOMMERCE_ID = 'woocommerce/woocommerce.php';
 	const WOOCOMMERCE_SLUG = 'woocommerce';
 
